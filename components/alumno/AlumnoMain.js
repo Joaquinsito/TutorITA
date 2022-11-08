63ded51d4ca8c21e1d7fbd6be6ac9ba2a77cf3e9
Joaquin Osuna <joaquin@MacBook-Pro-de-Joaquin.local>
2022-11-08 14:48:41 -0600

    Se soluciono el error en AlumnoMain

--- components/alumno/AlumnoMain.js
+++ components/alumno/AlumnoMain.js
@@ -1,9 +1,11 @@
-import React, { useEffect} from "react";
-import { Container, Center, Heading, Text, VStack, Box, FormControl, Input, Link, Button, HStack } from "native-base";
+import React, { useEffect, useState} from "react";
+import { Container, Center, Heading, Text, VStack, Box, FormControl, Input, Link, Button, HStack, Image } from "native-base";
 import axios from "axios";
 
 
 const AlumnoMain = ({route}) => {
+    const imageURI = require('../../assets/logoanimado2.gif');
+    const [isLoading, setLoading] = useState(true);
     const [formData, setFormData] = React.useState({});
     const [user, setUser] = React.useState({});
     const {noControl} = route.params;
@@ -11,42 +13,53 @@ const AlumnoMain = ({route}) => {
     
 
     useEffect(() => {
-        setFormData({...formData,
-            action: 'select'})
-        const formDataforRequest = new FormData()
-        formDataforRequest.append("noControl", '19150307')
-        formDataforRequest.append("action", formData.action)
-        const getData = async () => {
-            const response =  await axios.post(
-                'http://192.168.100.106:8888/tutorITA/api/api_alumno/select.php', 
-                formDataforRequest,
-                {Headers: {'Content-Type': 'multipart/form-data',
-                "Access-Control-Allow-Origin": "*"},
-                transformRequest: formData => formDataforRequest,}
-            ).then((response) => {
-                console.log(response.data);
-                setUser({...user, 
-                    noControl: response.data[0].noControl,
-                    nombreAlumno: response.data[0].nombreAlumno,
-                    apellidoAlumno: response.data[0].apellidoAlumno,
-                    idCarreraAlumno: response.data[0].idCarrera,
-                    emailAlumno: response.data[0].emailAlumno})
+        setTimeout(() => {
+            setFormData({...formData,
+                action: 'select'})
+            const formDataforRequest = new FormData()
+            formDataforRequest.append("noControl", '19150307')
+            formDataforRequest.append("action", formData.action)
+            const response =  axios.post(
+                    'http://192.168.100.61:80/tutorITA/api/api_alumno/select.php', 
+                    formDataforRequest,
+                    {Headers: {'Content-Type': 'multipart/form-data',
+                    "Access-Control-Allow-Origin": "*"},
+                    transformRequest: formData => formDataforRequest,}
+                ).then((response) => {
+                    console.log(response.data);
+                    setLoading(false);
+                    setUser({...user, 
+                        noControl: response.data[0].noControl,
+                        nombreAlumno: response.data[0].nombreAlumno,
+                        apellidoAlumno: response.data[0].apellidoAlumno,
+                        idCarreraAlumno: response.data[0].idCarrera,
+                        emailAlumno: response.data[0].emailAlumno});         
             });
-        }
+        }, 3000);
+    },[isLoading]);
 
-        getData()
-    },[])
+    if(isLoading){
+        return(
+            <Center>
+                <Image 
+            source={imageURI}
+            style={{width: 100, height: 100, justifyContent: 'center'}}
+            />
+            </Center>
+        )
+    }
 
 
-    return(
-        <Container>
-            <Text>{user.noControl}</Text>
-            <Text>{user.nombreAlumno}</Text>
-            <Text>{user.apellidoAlumno}</Text>
-            <Text>{user.idCarreraAlumno}</Text>
-            <Text>{user.emailAlumno}</Text>
-        </Container>
+    return (
+            <Container>
+                <Text>{user.noControl}</Text>
+                <Text>{user.nombreAlumno}</Text>
+                <Text>{user.apellidoAlumno}</Text>
+                <Text>{user.idCarreraAlumno}</Text>
+                <Text>{user.emailAlumno}</Text>
+            </Container> 
     )
+  
 }
 
 
