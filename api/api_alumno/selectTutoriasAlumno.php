7a879e77cdcae6d4d4a1e44941d8551db7a82482
Joaquin Osuna <joaquin@MacBook-Pro-de-Joaquin.local>
2022-11-20 22:42:28 -0600

    Se agrego la pantalla de home, carga datos desde bd

--- api/api_alumno/selectTutoriasAlumno.php
+++ api/api_alumno/selectTutoriasAlumno.php
@@ -5,7 +5,7 @@
     $action = $_POST['action'];
 
     if($action == 'select'){
-        $sql = "SELECT asesorias.idAsesoria, materias.idMateria, docentes.nombreDocente, asesorias.fecha, asesorias.hora, alumnos.nombreAlumno from asesorias INNER JOIN docentes ON asesorias.idDocenteAsesoria = docentes.idDocente INNER JOIN asesoriasAlumnos ON asesorias.idAsesoria = asesoriasAlumnos.idAsesoria INNER JOIN alumnos ON asesoriasAlumnos.idAlumno = alumnos.noControl INNER JOIN materias ON asesorias.idMateriaAsesoria = materias.idMateria where alumnos.noControl = '$idAlumno';";
+        $sql = "SELECT asesorias.idAsesoria, materias.nombreMateria, docentes.nombreDocente, asesorias.fecha, asesorias.hora, alumnos.nombreAlumno from asesorias INNER JOIN docentes ON asesorias.idDocenteAsesoria = docentes.idDocente INNER JOIN asesoriasAlumnos ON asesorias.idAsesoria = asesoriasAlumnos.idAsesoria INNER JOIN alumnos ON asesoriasAlumnos.idAlumno = alumnos.noControl INNER JOIN materias ON asesorias.idMateriaAsesoria = materias.idMateria where alumnos.noControl = '$idAlumno';";
 
         if($result = $conexion->query($sql)){
             for(
--- components/alumno/Home.js
+++ components/alumno/Home.js
@@ -1,18 +1,67 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Tutorias from '../tutorias/Tutorias.js';
+import axios from "axios";
+import { Center, NativeBaseProvider } from 'native-base';
 
-const Home = () => {
+
+export default function Home ({route}){
+  const [formData, setFormData] = useState({});
+  const [listTutorias, setlistTutorias] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const formDataforRequest = new FormData()
+    formDataforRequest.append("noControl", '18170306')
+    formDataforRequest.append("action", formData.action)
+
+    const getData = async () => {
+      setFormData({...formData,
+        action: 'select'})
+      
+      const response =  await axios.post(
+          'http://192.168.100.106:8888/tutorITA/api/api_alumno/selectTutoriasAlumno.php', 
+          formDataforRequest,
+          {Headers: {'Content-Type': 'multipart/form-data',
+          "Access-Control-Allow-Origin": "*"},
+          transformRequest: formData => formDataforRequest,}
+      )
+      setlistTutorias(response.data)
+      console.log("data responseed", listTutorias);
+    }
+
+    getData()
+    setLoading(false);
+},[isLoading]);
+
+
+
+
+const renderItem = ({ item }) => (
+    <Tutorias
+      idAsesoria = {item.idAsesoria}
+      nombreMateria = {item.nombreMateria}
+      nombreDocente= {item.nombreDocente}
+      fecha= {item.fecha}
+      hora= {item.hora}
+    />
+)
   return (
     <View
       style={{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: "white",
+        backgroundColor: "yellow",
       }}>
-      <Text>Home!</Text>
+      <Text>Tutorias!</Text>
+      <FlatList
+        style={{marginTop:15}}
+        data={listTutorias}
+        renderItem={renderItem}
+        keyExtractor={item => item.idAsesoria}
+      />
     </View>
-  );
-};
-
-export default Home;
+  )
+}
